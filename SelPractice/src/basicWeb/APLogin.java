83f8c06b4597125fb01c8ca4e467c75fec391cfa
Priyadarshini.Ramasa <Priyadarshini.Ramasa@KANINI-LTP-180.gisbizdev.gisbiz.chennai.com>
2019-12-03 16:35:12 +0530

    implicit wait

---
+++ SelPractice/src/basicWeb/APLogin.java
@@ -0,0 +1,25 @@
+package basicWeb;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class APLogin {
+
+	public static void main(String[] args) {
+		// http://chromedriver.storage.googleapis.com/index.html
+		//String baseURL = "http://automationpractice.com/index.php";
+		System.setProperty("webdriver.chrome.driver", "D:\\Desktopp\\Selenium\\libs\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.quit();
+		
+
+	}
+
+}
